daa7718148b750624fed1f36eec2d591d2992666
nft1-wirach <wsemwong@gmail.com>
2022-11-24 11:47:01 +0700

    not sure function

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ $monthNumber = ['Jan'=>1,'Feb'=>2,'Mar'=>3,'Apr'=>4,'May'=>5,'Jun'=>6,'Jul'=>7,'
 
 if(checkdate($monthNumber[$month], $day, $year)){
     echo "Your Weekday is ".weekDay($day,$month,$year);
+    echo "\r\n";
+    echo "Your Weekday2 is ".weekDay2($day,$month,$year);
 }else{
     echo "Incorrect date!!";
 }
@@ -39,5 +41,44 @@ function weekDay($day, $month, $year)
     
     $sumDate = ($day + $monthValue + ((($last2DigitYear + floor($last2DigitYear/4)) % 7) + $yearAdd)) % 7;
     
+    return $dayOfWeek[$sumDate];
+}
+
+function weekDay2($day, $month, $year)
+{
+    
+    $dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    $monthNumber = ['Jan'=>1,'Feb'=>2,'Mar'=>3,'Apr'=>4,'May'=>5,'Jun'=>6,'Jul'=>7,'Aug'=>8,'Sep'=>9,'Oct'=>10,'Nov'=>11,'Dec'=>12];
+    $monthAmount = [1=>31, 2=>28, 3=>31, 4=>30, 5=>31, 6=>30, 7=>31, 8=>31, 9=>30, 10=>31, 11=>30, 12=>31];
+
+    $startYear = 1900;
+    $leapYear = 0;
+
+    //$yearSubtract = $year > $startYear?1:0;
+    $yearSubtract = 0;
+    $countYear = ($year - $startYear) - $yearSubtract;
+    
+    for($i=$startYear; $i<=$year; $i++){
+        if((($i % 4) == 0) && ((($i % 100) != 0) || (($i %400) == 0))){
+            $leapYear++;
+        }  
+    }
+
+    if($monthNumber[$month] < 3){
+        $leapYear--;
+    }
+
+    $dayInYear = ($countYear*365) + $leapYear;
+
+    $dayInMonth = 0;
+    $countMonth = ($monthNumber[$month] - 1);
+    for($x = 1; $x <= $countMonth; $x++){
+        $dayInMonth += $monthAmount[$x];
+    }
+
+    //$dayInMonth = ($monthNumber[$month] - 1) * 30;
+    //echo "$leapYear $countYear ($day + $dayInMonth + $dayInYear)".($day + $dayInMonth + $dayInYear) ."--".($day + $dayInMonth + $dayInYear) % 7;
+    $sumDate = ($day + $dayInMonth + $dayInYear) % 7;
+
     return $dayOfWeek[$sumDate];
 }
